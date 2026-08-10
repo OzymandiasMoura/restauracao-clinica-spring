93743f8c9f8aa15182a0d005c287ed0bb468f5a4
OzymandiasMoura <pedro.hcmoura11@gmail.com>
2026-08-10 11:50:25 -0300

    feat: criar configuração de projeto

---
+++ src/main/java/org/example/clinicarestauracao/ClinicaRestauracaoApplication.java
@@ -0,0 +1,15 @@
+package org.example.clinicarestauracao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaRestauracaoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ClinicaRestauracaoApplication.class, args);
+    }
+
+}
