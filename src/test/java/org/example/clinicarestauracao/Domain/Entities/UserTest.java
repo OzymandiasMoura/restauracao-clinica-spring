22f551eed104576156de64e8034ecccaa9fc2165
OzymandiasMoura <pedro.hcmoura11@gmail.com>
2026-08-17 13:26:27 -0300

    feat: criar testes das exceções

--- src/test/java/org/example/clinicarestauracao/Domain/Entities/UserTest.java
+++ src/test/java/org/example/clinicarestauracao/Domain/Entities/UserTest.java
@@ -1,10 +1,18 @@
 package org.example.clinicarestauracao.Domain.Entities;
 
+import org.example.clinicarestauracao.Application.Exceptions.UserWithInvalidInformationException;
 import org.example.clinicarestauracao.Builders.UserTestBuilder;
 import org.example.clinicarestauracao.Domain.Enums.UserRoles;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.security.core.GrantedAuthority;
+
+import java.util.stream.Stream;
+
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class UserTest
 {
@@ -29,4 +37,32 @@ class UserTest
 
         assertThat(authorities).containsExactly("ROLE_ADMIN", "ROLE_USER");
     }
+
+    @ParameterizedTest(name = "{index}")
+    @MethodSource(value = "dataProvider")
+    void shouldThrowExceptionWhenUserHasNullInformation(Long id, String username, String password, UserRoles role, String message)
+    {
+        UserWithInvalidInformationException e = assertThrows(UserWithInvalidInformationException.class, () ->
+                new User(username, password, role));
+
+        assertThat(e.getMessage()).isEqualTo(message);
+    }
+
+    private static Stream<Arguments> dataProvider()
+    {
+        return Stream.of(
+                Arguments.of(1L, null, "123", UserRoles.ADMIN, "Nome de usuário não pode ser vazio."),
+                Arguments.of(1L, "Pedro", null, UserRoles.ADMIN, "Senha de usuário não pode ser vazio."),
+                Arguments.of(1L, "Pedro", "123", null, "Papel do usuário deve ser definido.")
+        );
+    }
+
+    private static Stream<Arguments> dataProvider2()
+    {
+        return Stream.of(
+                Arguments.of(1L, " ", "123", UserRoles.ADMIN, "Nome de usuário não pode ser vazio."),
+                Arguments.of(1L, "Pedro", " ", UserRoles.ADMIN, "Senha de usuário não pode ser vazio.")
+        );
+    }
+
 }
