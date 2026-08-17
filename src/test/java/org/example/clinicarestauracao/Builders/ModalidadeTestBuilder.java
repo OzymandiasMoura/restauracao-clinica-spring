87e82b85604b420f7fcfa09358c489dcc0975423
OzymandiasMoura <pedro.hcmoura11@gmail.com>
2026-08-17 16:10:33 -0300

    feat: criar builder de modalidade

---
+++ src/test/java/org/example/clinicarestauracao/Builders/ModalidadeTestBuilder.java
@@ -0,0 +1,55 @@
+package org.example.clinicarestauracao.Builders;
+
+import org.example.clinicarestauracao.Domain.Entities.Modalidade;
+
+public class ModalidadeTestBuilder
+{
+    Long id = 1L;
+    String descricao = "Modalidade";
+    String cnpj = "11222333000181";
+    int maxVagas = 20;
+    boolean ativo = true;
+    boolean pagamento = true;
+
+    public ModalidadeTestBuilder setId(Long id)
+    {
+        this.id = id;
+        return this;
+    }
+
+    public ModalidadeTestBuilder setDescricao(String descricao)
+    {
+        this.descricao = descricao;
+        return this;
+    }
+
+    public ModalidadeTestBuilder setCNPJ(String cnpj)
+    {
+        this.cnpj = cnpj;
+        return this;
+    }
+
+    public ModalidadeTestBuilder setMaxVagas(int maxVagas)
+    {
+        this.maxVagas = maxVagas;
+        return this;
+    }
+
+    public ModalidadeTestBuilder setAtivo(boolean ativo)
+    {
+        this.ativo = ativo;
+        return this;
+    }
+
+    public ModalidadeTestBuilder setPagamento(boolean pagamento)
+    {
+        this.pagamento = pagamento;
+        return this;
+    }
+
+    public Modalidade build()
+    {
+        Modalidade mod = new Modalidade(id, descricao, cnpj, maxVagas, ativo, pagamento);
+        return mod;
+    }
+}
