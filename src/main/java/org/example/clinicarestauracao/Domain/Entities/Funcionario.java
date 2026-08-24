975f08a6917a2c39d6322ab9d861f4abba72cdd9
OzymandiasMoura <pedro.hcmoura11@gmail.com>
2026-08-24 16:43:51 -0300

    feat: criar funções para cpf e seus testes

--- src/main/java/org/example/clinicarestauracao/Domain/Entities/Funcionario.java
+++ src/main/java/org/example/clinicarestauracao/Domain/Entities/Funcionario.java
@@ -2,11 +2,15 @@ package org.example.clinicarestauracao.Domain.Entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.clinicarestauracao.Application.Exceptions.FuncionarioWithInvalidInformationException;
+import org.example.clinicarestauracao.Domain.Validation.CpfValidator;
 
 import java.time.LocalDate;
 
 @Entity
 @Data
+@NoArgsConstructor
 @Table(name = "Funcionarios")
 public class Funcionario
 {
@@ -27,12 +31,77 @@ public class Funcionario
     @JoinColumn(name = "user_id", nullable = true)
     private User user;
 
+    public Funcionario(Long id, String nome, String cpf,  String email, LocalDate dataNascimento, Boolean ativo, User user)
+    {
+        setId(id);
+        setNome(nome);
+        setCpf(cpf);
+        setEmail(email);
+        setDataNascimento(dataNascimento);
+        setAtivo(ativo);
+        setUser(user);
+    }
+
+    public Funcionario(String nome, String cpf,  String email, LocalDate dataNascimento, Boolean ativo, User user)
+    {
+        setNome(nome);
+        setCpf(cpf);
+        setEmail(email);
+        setDataNascimento(dataNascimento);
+        setAtivo(ativo);
+        setUser(user);
+    }
+
+    public Funcionario(Long id, String nome, String cpf,  String email, LocalDate dataNascimento, Boolean ativo)
+    {
+        setId(id);
+        setNome(nome);
+        setCpf(cpf);
+        setEmail(email);
+        setDataNascimento(dataNascimento);
+        setAtivo(ativo);
+    }
+
+    public Funcionario(String nome, String cpf,  String email, LocalDate dataNascimento, Boolean ativo)
+    {
+        setNome(nome);
+        setCpf(cpf);
+        setEmail(email);
+        setDataNascimento(dataNascimento);
+        setAtivo(ativo);
+    }
+
     public void setNome(String nome)
     {
         if (nome == null || nome.isBlank())
         {
-
+            throw new FuncionarioWithInvalidInformationException("Funcionário não pode ter o nome vazio ou em branco.");
         }
-        this.nome = nome;
+        if (nome.length() < 3)
+        {
+            throw new FuncionarioWithInvalidInformationException("Nome deve ter no mínimo 3 caracteres.");
+        }
+        else
+        {
+            this.nome = nome;
+        }
+    }
+
+    public void setCpf(String cpf)
+    {
+        String cpfNormalizado = CpfValidator.normalize(cpf);
+
+        if(cpfNormalizado == null || cpfNormalizado.isBlank())
+        {
+            throw new FuncionarioWithInvalidInformationException("CPF não pode ser vazio ou em branco.");
+        }
+        if(!CpfValidator.validate(cpfNormalizado))
+        {
+            throw new FuncionarioWithInvalidInformationException("CPF é inválido.");
+        }
+        else
+        {
+            this.cpf = cpfNormalizado;
+        }
     }
 }
---
+++ src/test/java/org/example/clinicarestauracao/Domain/Entities/FuncionarioTest.java
@@ -0,0 +1,72 @@
+package org.example.clinicarestauracao.Domain.Entities;
+
+import org.example.clinicarestauracao.Application.Exceptions.FuncionarioWithInvalidInformationException;
+import org.example.clinicarestauracao.Builders.FuncionarioTestBuilder;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FuncionarioTest
+{
+    //Testes para o nome
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "   "})
+    void shouldRejectNullOrBlankName(String nome)
+    {
+        var exception = assertThrows(FuncionarioWithInvalidInformationException.class, () -> FuncionarioTestBuilder.newFuncionario().setNome(nome).build());
+
+        assertEquals("Funcionário não pode ter o nome vazio ou em branco.", exception.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"A", "Pe"})
+    void shouldRejectNameWithLessThanThreeCharacters(String nome)
+    {
+        var exception = assertThrows(FuncionarioWithInvalidInformationException.class, () -> FuncionarioTestBuilder.newFuncionario().setNome(nome).build());
+
+        assertEquals("Nome deve ter no mínimo 3 caracteres.", exception.getMessage());
+    }
+
+    @Test
+    void shouldCreateFuncionarioWithValidName()
+    {
+        Funcionario funcionario = FuncionarioTestBuilder.newFuncionario().setNome("Pedro Moura").build();
+
+        assertEquals("Pedro Moura", funcionario.getNome());
+    }
+
+    //Testes para o CPF
+
+    @Test
+    void shouldNormalizeCpf()
+    {
+        Funcionario funcionario = FuncionarioTestBuilder.newFuncionario().setCpf("529.982.247-25").build();
+
+        assertEquals("52998224725", funcionario.getCpf());
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {" ", ""})
+    void shouldRejectNullOrBlankCpf(String cpf)
+    {
+        var exception = assertThrows(FuncionarioWithInvalidInformationException.class, () -> FuncionarioTestBuilder.newFuncionario().setCpf(cpf).build());
+
+        assertEquals("CPF não pode ser vazio ou em branco.", exception.getMessage());
+    }
+
+    @Test
+    void shouldRejectInvalidCpf()
+    {
+        FuncionarioWithInvalidInformationException ex = assertThrows(FuncionarioWithInvalidInformationException.class, () -> FuncionarioTestBuilder.newFuncionario().setCpf("529.982.247-24").build());
+
+        assertEquals("CPF é inválido.", ex.getMessage());
+    }
+
+}
