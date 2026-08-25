e23f4e1455c4b0f868555e44755b4a7abb43429a
LanaMoraes06 <lanaalvesdemoraes83.com>
2026-08-25 09:36:36 -0300

    feat: implementar validações do medicamento

--- src/main/java/org/example/clinicarestauracao/Domain/Entities/Medicamento.java
+++ src/main/java/org/example/clinicarestauracao/Domain/Entities/Medicamento.java
@@ -5,11 +5,11 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.clinicarestauracao.Application.Exceptions.Medicamento.MedicamentoWithInvalidInformationException;
 
 @Entity
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "Medicamento")
 public class Medicamento {
 
@@ -23,6 +23,33 @@ public class Medicamento {
     @Column(name = "preco", nullable = false)
     private double preco;
 
+    public Medicamento(Long id, String nome, double preco) {
+        this.id = id;
+        setNome(nome);
+        setPreco(preco);
+    }
 
+    public Medicamento(String nome, double preco) {
+        setNome(nome);
+        setPreco(preco);
+    }
+
+    public void setNome(String nome){
+        if (nome == null || nome.isBlank()){
+            throw new MedicamentoWithInvalidInformationException("O nome não pode ser nulo ou vazia.");
+        } else if(nome.length() < 3){
+            throw new MedicamentoWithInvalidInformationException("O nome não pode ser menor que 3 caracteres.");
+        } else{
+            this.nome = nome;
+        }
+    }
+
+    public void setPreco(double preco){
+        if (preco < 0){
+            throw new MedicamentoWithInvalidInformationException("O preço não pode ser negativo.");
+        } else {
+            this.preco = preco;
+        }
+    }
 
 }
