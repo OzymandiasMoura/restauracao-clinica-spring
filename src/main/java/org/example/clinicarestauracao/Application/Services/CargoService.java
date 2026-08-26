6046812c5719a233dc73faa310d7d75f54d64e05
OzymandiasMoura <pedro.hcmoura11@gmail.com>
2026-08-26 14:30:41 -0300

    feat: construir metodo findCargoByName  e seus testes no service

--- src/main/java/org/example/clinicarestauracao/Application/Services/CargoService.java
+++ src/main/java/org/example/clinicarestauracao/Application/Services/CargoService.java
@@ -1,5 +1,6 @@
 package org.example.clinicarestauracao.Application.Services;
 
+import org.example.clinicarestauracao.Application.Exceptions.Cargo.CargoNotFoundException;
 import org.example.clinicarestauracao.Application.Exceptions.Cargo.CargoWithInvalidInformationException;
 import org.example.clinicarestauracao.Application.Interfaces.CargoRepository;
 import org.example.clinicarestauracao.Domain.Entities.Cargo;
@@ -22,5 +23,17 @@ public class CargoService
             throw new CargoWithInvalidInformationException("Nome do cargo já existente.");
         }
         return repository.save(new Cargo(cargo.getNome()));
+    }
+
+    public Cargo findCargoByNome(String nome)
+    {
+        if(nome == null || nome.isBlank())
+        {
+            throw new CargoNotFoundException("Nome do cargo não pode ser nulo ou vazio.");
+        }
+
+        String nomeNormalizado = nome.strip();
+
+        return repository.findCargoByNome(nomeNormalizado).orElseThrow(() -> new CargoNotFoundException("Cargo não encontrado."));
     }
 }
--- src/test/java/org/example/clinicarestauracao/Application/Services/CargoServiceTest.java
+++ src/test/java/org/example/clinicarestauracao/Application/Services/CargoServiceTest.java
@@ -1,11 +1,14 @@
 package org.example.clinicarestauracao.Application.Services;
 
+import org.example.clinicarestauracao.Application.Exceptions.Cargo.CargoNotFoundException;
 import org.example.clinicarestauracao.Application.Exceptions.Cargo.CargoWithInvalidInformationException;
 import org.example.clinicarestauracao.Application.Interfaces.CargoRepository;
 import org.example.clinicarestauracao.Builders.CargoTestBuilder;
 import org.example.clinicarestauracao.Domain.Entities.Cargo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -23,6 +26,8 @@ class CargoServiceTest
     private CargoRepository repository;
     @InjectMocks
     private CargoService service;
+
+    //Testes para o createCargo
 
     @Test
     void shouldCreateCargoSuccessfully()
@@ -58,11 +63,69 @@ class CargoServiceTest
 
         Mockito.when(repository.findCargoByNome(entry.getNome())).thenReturn(Optional.of(existed));
 
-        CargoWithInvalidInformationException ex =  assertThrows(CargoWithInvalidInformationException.class, () -> service.createCargo(entry));
+        CargoWithInvalidInformationException ex = assertThrows(CargoWithInvalidInformationException.class, () -> service.createCargo(entry));
 
         assertEquals("Nome do cargo já existente.", ex.getMessage());
 
         Mockito.verify(repository).findCargoByNome(entry.getNome());
         Mockito.verify(repository, Mockito.never()).save(Mockito.any(Cargo.class));
     }
+
+    //Testes para findByNome
+
+    @Test
+    void shouldFindCargoByNameSuccessfully()
+    {
+        String name = "Monitor";
+        Cargo existed = CargoTestBuilder.newCargo().build();
+
+        Mockito.when(repository.findCargoByNome(name)).thenReturn(Optional.of(existed));
+
+        Cargo response = service.findCargoByNome(name);
+
+        assertNotNull(response);
+        assertSame(existed, response);
+        assertEquals(1L, response.getId());
+        assertEquals("Monitor", response.getNome());
+
+        Mockito.verify(repository).findCargoByNome(name);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenCargoIsNotFoundByName()
+    {
+        Mockito.when(repository.findCargoByNome("Monitor")).thenReturn(Optional.empty());
+
+        CargoNotFoundException ex = assertThrows(CargoNotFoundException.class, () -> service.findCargoByNome("Monitor"));
+
+        assertEquals("Cargo não encontrado.", ex.getMessage());
+
+        Mockito.verify(repository).findCargoByNome("Monitor");
+    }
+
+    @Test
+    void shouldNormalizeNameWhenFindingCargoByName()
+    {
+        Cargo existed = CargoTestBuilder.newCargo().build();
+
+        Mockito.when(repository.findCargoByNome("Monitor")).thenReturn(Optional.of(existed));
+
+        Cargo response = service.findCargoByNome("  Monitor  ");
+
+        assertSame(existed, response);
+        Mockito.verify(repository).findCargoByNome("Monitor");
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "    "})
+    void shouldThrowExceptionWhenNameIsEmptyOrBlank(String nome)
+    {
+        CargoNotFoundException exception = assertThrows(CargoNotFoundException.class, () -> service.findCargoByNome(nome));
+
+        assertEquals("Nome do cargo não pode ser nulo ou vazio.", exception.getMessage());
+
+        Mockito.verifyNoInteractions(repository);
+    }
+
 }
